11888bf042f6b064b46da47970d5e31a4f01c62a
Mepy <angel@mepy.net>
2022-01-14 22:27:56 +0800

    ast v0.0.2

--- src/ast.mjs
+++ src/ast.mjs
@@ -1,12 +1,23 @@
 // 2022/01/12 23:30-24:33
+// 2022/01/13 10:31-10:41
+// 2022/01/14 22:10-
+
+/* --- Type --- */
+const bool_type = ({tag:"bool_ty",body:undefined}) 
+const char_type = ({tag:"char_ty",body:undefined})
+const int_type = ({tag:"int_ty",body:undefined})
+const float_type = ({tag:"float_ty",body:undefined})
+/* --- Type --- */
+
 
 /* --- Term --- */
 // literal
-const bool_gen = bool=>({tag:"bool",body:{bool}})
-const char_gen = char=>({tag:"char",body:{char}})
-const int_gen = int=>({tag:"int",body:{int}})
-const float_gen = float=>({tag:"float",body:{float}})
+const bool_gen = bool=>({tag:"bool",body:{bool},type:bool_type})
+const char_gen = char=>({tag:"char",body:{char},type:char_type})
+const int_gen = int=>({tag:"int",body:{int},type:int_type})
+const float_gen = float=>({tag:"float",body:{float},type:float_type})
 const array_gen = array=>({tag:"array",body:{array}})
+const list_gen = list=>({tag:"list",body:{list}}) // linked list implemented by polymorphism
 const tuple_gen = tuple=>({tag:"tuple",body:{tuple}})
 const dict_gen = record=>({tag:"dict",body:{record}}) // {key:value;key:value}
 const block_gen = block=>({tag:"block",body:{block}}) // code block = {term;term;term;}
@@ -24,6 +35,18 @@ const sub_gen = (left,right)=>({tag:"sub",body:{left,right}})
 const mul_gen = (left,right)=>({tag:"mul",body:{left,right}})
 const div_gen = (left,right)=>({tag:"div",body:{left,right}})
 const mod_gen = (left,right)=>({tag:"mod",body:{left,right}})
+
+// binary operation - compare
+const eq_gen = (left,right)=>({tag:"eq",body:{left,right}})
+const neq_gen = (left,right)=>({tag:"neq",body:{left,right}})
+const lt_gen = (left,right)=>({tag:"lt",body:{left,right}})
+const leq_gen = (left,right)=>({tag:"leq",body:{left,right}})
+const gt_gen = (left,right)=>({tag:"gt",body:{left,right}})
+const geq_gen = (left,right)=>({tag:"geq",body:{left,right}})
+
+// binary operation - stream
+const sl_gen = (left,right)=>({tag:"sl",body:{left,right}}) // left "<<" left
+const sr_gen = (left,right)=>({tag:"sr",body:{left,right}}) // left ">>" right
 
 // binary operation - function
 const fun_gen = (arg,ret)=>({tag:"fun",body:{arg,ret}})
@@ -64,9 +87,11 @@ const arrow_gen = (arg,ret)=>({tag:"arrow",body:{arg,ret}})
 const pow_gen = (type,length)=>({tag:"pow",body:{type,length}})
 const record_gen = (keys,types)=>({tag:"record",body:{keys,types}})
 
-const alias_gen = (name,type)=>({tag:"alias",body:{name,type}}) // type alias
-const wrap_gen = (name,type)=>({tag:"wrap",body:{name,type}}) // type wrap
+const alias_gen = (name,type)=>({tag:"alias",body:{name,type}}) // type alias  Alias := Int
+const wrap_gen = (name,type)=>({tag:"wrap",body:{name,type}}) // type wrap     Wrap = Int
 
+
+// to support c's pointer or cpp's reference
 const ptr_gen = type=>({tag:"ptr",body:{type}}) // only mutable
 const ref_gen = type=>({tag:"ref",body:{type}}) // only immutable
 
@@ -75,8 +100,8 @@ const ref_gen = type=>({tag:"ref",body:{type}}) // only immutable
 /* --- Type --- */
 
 /* --- Term : Type --- */
-// We just make type become a sub field of body
-// i.e let x : Int = 2 -> ast = {tag:"let",body:{int:2,type:Int}} 
+// We just make type become a sub field of term
+// i.e let x : Int = 2 -> ast = {tag:"let",body:{int:2},type:Int} 
 
 /* --- Module --- */
-// TODO
+// TODO with universal and existential types
